46aa1e2b6c02da282b009baa8a32800bfe1bb0d7
Ericgacoki <gacokieric@gmail.com>
2022-05-26 15:05:23 +0300

    app module

---
+++ app/src/main/java/com/ericg/jichat/di/AppModule.kt
@@ -0,0 +1,54 @@
+package com.ericg.jichat.di
+
+import com.ericg.jichat.data.remote.APIService
+import com.ericg.jichat.data.repository.ChatRepository
+import com.ericg.jichat.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Singleton
+    @Provides
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .callTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesAPIService(okHttpClient: OkHttpClient): APIService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(APIService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun providesChatRepository(apiService: APIService): ChatRepository {
+        return ChatRepository(apiService)
+    }
+}
