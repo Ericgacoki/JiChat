66c0719d8d8530477aab9cb138a3e56ce57ac431
Ericgacoki <gacokieric@gmail.com>
2022-05-28 04:57:46 +0300

    rename APIService to ApiService

--- app/src/main/java/com/ericg/jichat/data/remote/ApiService.kt
+++ app/src/main/java/com/ericg/jichat/data/remote/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-interface APIService {
+interface ApiService {
     // http://api.brainshop.ai/get?bid=156451&key=RdjOJWhMeR2JDdOW&uid=%5Buid%5D&msg=hey
     @GET("get?")
     suspend fun getBotResponse(
