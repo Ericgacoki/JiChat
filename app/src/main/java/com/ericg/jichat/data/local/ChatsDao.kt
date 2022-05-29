06d39991c825852cf212f89f442e59ad7333602d
Ericgacoki <gacokieric@gmail.com>
2022-05-29 04:58:29 +0300

    add chats dao

--- app/src/main/java/com/ericg/jichat/data/local/ChatsDao.kt
+++ app/src/main/java/com/ericg/jichat/data/local/ChatsDao.kt
@@ -1,4 +1,17 @@
 package com.ericg.jichat.data.local
 
+import androidx.room.*
+import com.ericg.jichat.model.Chat
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface ChatsDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun saveChat(chat: Chat)
+
+    @Query("SELECT * FROM chats ORDER BY id ASC")
+    fun getChats(): Flow<List<Chat>>
+
+    @Query("DELETE FROM chats")
+    suspend fun deleteChats()
 }
