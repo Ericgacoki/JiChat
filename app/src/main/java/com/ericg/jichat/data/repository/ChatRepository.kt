ecba1fbabf1b92ebc0844a9759b93ce6f74e0d8e
Ericgacoki <gacokieric@gmail.com>
2022-05-29 04:59:14 +0300

    add chats data access methods

--- app/src/main/java/com/ericg/jichat/data/repository/ChatRepository.kt
+++ app/src/main/java/com/ericg/jichat/data/repository/ChatRepository.kt
@@ -1,11 +1,14 @@
 package com.ericg.jichat.data.repository
 
-import com.ericg.jichat.data.remote.APIService
+import com.ericg.jichat.data.local.ChatDatabase
+import com.ericg.jichat.data.remote.ApiService
 import com.ericg.jichat.data.remote.BotResponse
+import com.ericg.jichat.model.Chat
 import com.ericg.jichat.util.Resource
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class ChatRepository @Inject constructor(private val apiService: APIService) {
+class ChatRepository @Inject constructor(private val apiService: ApiService, private val chatsDatabase: ChatDatabase) {
     suspend fun getBotResponse(message: String): Resource<BotResponse>{
         val response = try {
             apiService.getBotResponse(message = message)
@@ -13,5 +16,17 @@ class ChatRepository @Inject constructor(private val apiService: APIService) {
             return Resource.Error(message = "Failed to load bot response")
         }
         return Resource.Success(response)
+    }
+
+    suspend fun saveChat(chat: Chat){
+        chatsDatabase.chatsDao.saveChat(chat)
+    }
+
+    fun getChats(): Flow<List<Chat>> {
+        return chatsDatabase.chatsDao.getChats()
+    }
+
+    suspend fun deleteChats(){
+        chatsDatabase.chatsDao.deleteChats()
     }
 }
