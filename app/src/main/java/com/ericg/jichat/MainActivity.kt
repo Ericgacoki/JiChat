48fe340d16ea69cdad86557861145b9acff4f720
Ericgacoki <gacokieric@gmail.com>
2022-05-30 17:04:18 +0300

    update main activity to use material3

--- app/src/main/java/com/ericg/jichat/MainActivity.kt
+++ app/src/main/java/com/ericg/jichat/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Delete
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.*
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ericg.jichat.screens.ChatScreen
 import com.ericg.jichat.ui.theme.JiChatTheme
@@ -24,47 +24,48 @@ import java.time.format.DateTimeFormatter
 @Suppress("DEPRECATION") // 🤪
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
 
+
         setContent {
             val chatsViewModel: ChatViewModel = hiltViewModel()
+
             JiChatTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    val scaffoldState = rememberScaffoldState()
                     Scaffold(
                         modifier = Modifier.fillMaxSize(),
-                        scaffoldState = scaffoldState,
                         topBar = {
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.SpaceBetween,
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Text(
-                                    text = "JiChat",
-                                    fontSize = 24.sp,
-                                    fontWeight = FontWeight.SemiBold,
-                                    modifier = Modifier.padding(horizontal = 12.dp)
-                                )
-                                IconButton(
-                                    modifier = Modifier.padding(horizontal = 12.dp),
-                                    onClick = {
-                                        chatsViewModel.deleteChats()
-                                    }) {
-                                    Icon(
-                                        imageVector = Icons.Rounded.Delete,
-                                        contentDescription = "delete icon"
+                            SmallTopAppBar(
+                                title = {
+                                    Text(
+                                        text = "JiChat",
+                                        style = MaterialTheme.typography.titleLarge
                                     )
+                                },
+                                colors = TopAppBarDefaults.smallTopAppBarColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant
+                                ), actions = {
+                                    IconButton(
+                                        modifier = Modifier.padding(horizontal = 12.dp),
+                                        onClick = {
+                                            chatsViewModel.deleteChats()
+                                        }) {
+                                        Icon(
+                                            imageVector = Icons.Rounded.Delete,
+                                            contentDescription = "delete icon"
+                                        )
+                                    }
                                 }
-                            }
+                            )
                         }
                     ) {
-                        ChatScreen()
+                        ChatScreen(paddingValues = it)
                     }
                 }
             }
